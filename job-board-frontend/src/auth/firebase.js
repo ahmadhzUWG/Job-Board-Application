e798b7beed081a46a4f53190287a92c179560d8a
Ahmad H <ah00274@my.westga.edu>
2025-09-16 17:24:52 -0400

    Added env. local variables

--- job-board-frontend/src/auth/firebase.js
+++ job-board-frontend/src/auth/firebase.js
@@ -6,14 +6,12 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBISlkctSvxhF3SeWHlneCJOzhz_jT5p9s",
-  authDomain: "job-board-f8219.firebaseapp.com",
-  projectId: "job-board-f8219",
-  storageBucket: "job-board-f8219.firebasestorage.app",
-  messagingSenderId: "1000699416593",
-  appId: "1:1000699416593:web:821d22bbfae58e0ff9e1b0",
-  measurementId: "G-NGMN0VJ6TZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
